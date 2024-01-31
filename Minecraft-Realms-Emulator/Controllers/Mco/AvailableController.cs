4d5d9b0495399330016fed20b5dafe36ef80ceea
CyberL1 <cyber.hf18@gmail.com>
2024-01-31 15:50:15 +0100

    feat: GET /mco/available, GET /mco/client/compatible

---
+++ Minecraft-Realms-Emulator/Controllers/Mco/AvailableController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Minecraft_Realms_Emulator.Controllers.Mco
+{
+    [Route("mco/[controller]")]
+    [ApiController]
+    public class AvailableController : ControllerBase
+    {
+        [HttpGet(Name = "GetAvailable")]
+        public bool Get()
+        { 
+            return true;
+        }
+    }
+}
---
+++ Minecraft-Realms-Emulator/Controllers/Mco/Client/CompatibleController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Minecraft_Realms_Emulator.Controllers.Mco.Client
+{
+    [Route("/mco/client/[controller]")]
+    [ApiController]
+    public class CompatibleController : ControllerBase
+    {
+        [HttpGet(Name = "GetCompatible")]
+        public string Get()
+        {
+            return Compatility.COMPATIBLE.ToString();
+        }
+    }
+}
