b529080d6504b31a20392036456acfb1efd0548e
CyberL1 <cyber.hf18@gmail.com>
2024-01-31 16:07:44 +0100

    feat: GET /trial

---
+++ Minecraft-Realms-Emulator/Controllers/TrialController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Minecraft_Realms_Emulator.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TrialController : ControllerBase
+    {
+        [HttpGet(Name = "GetTrial")]
+        public bool Get() { 
+            return true;
+        }
+    }
+}
