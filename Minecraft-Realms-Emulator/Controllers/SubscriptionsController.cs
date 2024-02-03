8240cd43a4a2767484ed2ad7cd8667377622efdb
CyberL1 <cyber.hf18@gmail.com>
2024-02-03 14:27:05 +0100

    feat: subscriptions

---
+++ Minecraft-Realms-Emulator/Controllers/SubscriptionsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Minecraft_Realms_Emulator.Data;
+using Minecraft_Realms_Emulator.Entities;
+
+namespace Minecraft_Realms_Emulator.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SubscriptionsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public SubscriptionsController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Subscription>> Get(int id)
+        {
+            var world = await _context.Worlds.FindAsync(id);
+            var subscriptions = await _context.Subscriptions.ToListAsync();
+
+            if (world == null) return NotFound("Subscription njot found");
+
+            var subscription = subscriptions.Find(s => s.RemoteId == world.RemoteSubscriptionId);
+
+            return Ok(subscription);
+        }
+    }
+}
---
+++ Minecraft-Realms-Emulator/Migrations/20240203132410_Subscriptions.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Minecraft_Realms_Emulator.Migrations
+{
+    /// <inheritdoc />
+    public partial class Subscriptions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RemoteSubscriptionId",
+                table: "Worlds",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Subscriptions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    RemoteId = table.Column<string>(type: "text", nullable: false),
+                    StartDate = table.Column<string>(type: "text", nullable: false),
+                    DaysLeft = table.Column<int>(type: "integer", nullable: false),
+                    SubscriptionType = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subscriptions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Subscriptions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RemoteSubscriptionId",
+                table: "Worlds",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
