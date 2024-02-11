71823e4c6f551f00c5f1500423b7c09c147983eb
CyberL1 <cyber.hf18@gmail.com>
2024-02-11 15:10:34 +0100

    feat: world slots

---
+++ Minecraft-Realms-Emulator/Entities/Slot.cs
@@ -0,0 +1,8 @@
+﻿namespace Minecraft_Realms_Emulator.Entities
+{
+    public class Slot
+    {
+        public int SlotId { get; set; }
+        public string Options { get; set; } = "{}";
+    }
+}
---
+++ Minecraft-Realms-Emulator/Migrations/20240211135246_Slots.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Minecraft_Realms_Emulator.Migrations
+{
+    /// <inheritdoc />
+    public partial class Slots : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER TABLE \"Worlds\" ALTER COLUMN \"Slots\" TYPE jsonb[] USING \"Slots\"::jsonb[]");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER TABLE \"Worlds\" ALTER COLUMN \"Slots\" TYPE text[] USING \"Slots\"::text[]");
+        }
+    }
+}
