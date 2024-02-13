881576693208472d92458d5b6ab726a6d716fecf
CyberL1 <cyber.hf18@gmail.com>
2024-02-13 11:54:29 +0100

    feat: world joining

--- Minecraft-Realms-Emulator/Migrations/20240213104200_Connections.Designer.cs
+++ Minecraft-Realms-Emulator/Migrations/20240213104200_Connections.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Minecraft_Realms_Emulator.Data;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Minecraft_Realms_Emulator.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240213104200_Connections")]
+    partial class Connections
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
