5dd9a261fe8294ac300528f6771c28add0b4160f
CyberL1 <mateusz.hf18@gmail.com>
2024-02-13 11:54:29 +0100

    feat: world joining

---
+++ Minecraft-Realms-Emulator/Entities/Connection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Minecraft_Realms_Emulator.Entities
+{
+    [Keyless]
+    public class Connection
+    {
+        public World World { get; set; }
+        public string Address { get; set; } = string.Empty;
+        public bool PendingUpdate { get; set; }
+    }
+}
---
+++ Minecraft-Realms-Emulator/Migrations/20240213104200_Connections.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Minecraft_Realms_Emulator.Migrations
+{
+    /// <inheritdoc />
+    public partial class Connections : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Connections",
+                columns: table => new
+                {
+                    WorldId = table.Column<int>(type: "integer", nullable: false),
+                    Address = table.Column<string>(type: "text", nullable: false),
+                    PendingUpdate = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.ForeignKey(
+                        name: "FK_Connections_Worlds_WorldId",
+                        column: x => x.WorldId,
+                        principalTable: "Worlds",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Connections_WorldId",
+                table: "Connections",
+                column: "WorldId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Connections");
+        }
+    }
+}
