f4a508489fce7f410189f6f63bcd9ea82e870103
CyberL1 <mateusz.hf18@gmail.com>
2024-02-14 16:49:19 +0100

    feat: backups

---
+++ Minecraft-Realms-Emulator/Entities/Backup.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace Minecraft_Realms_Emulator.Entities
+{
+    public class Backup
+    {
+        public int Id { get; set; }
+        public World World { get; set; }
+        public string BackupId { get; set; }
+        public long LastModifiedDate { get; set; }
+        public int Size { get; set; }
+        public JsonDocument Metadata { get; set; }
+    }
+}
---
+++ Minecraft-Realms-Emulator/Migrations/20240214154708_Backups.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Minecraft_Realms_Emulator.Migrations
+{
+    /// <inheritdoc />
+    public partial class Backups : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Backups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    WorldId = table.Column<int>(type: "integer", nullable: false),
+                    BackupId = table.Column<string>(type: "text", nullable: false),
+                    LastModifiedDate = table.Column<long>(type: "bigint", nullable: false),
+                    Size = table.Column<int>(type: "integer", nullable: false),
+                    Metadata = table.Column<JsonDocument>(type: "jsonb", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Backups", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Backups_Worlds_WorldId",
+                        column: x => x.WorldId,
+                        principalTable: "Worlds",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Backups_WorldId",
+                table: "Backups",
+                column: "WorldId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Backups");
+        }
+    }
+}
