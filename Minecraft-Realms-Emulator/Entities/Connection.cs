e52fead68c6dd7de55baf491da0541db82d7e641
CyberL1 <cyber.hf18@gmail.com>
2024-02-18 17:04:16 +0100

    feat: connections id

--- Minecraft-Realms-Emulator/Entities/Connection.cs
+++ Minecraft-Realms-Emulator/Entities/Connection.cs
@@ -1,10 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace Minecraft_Realms_Emulator.Entities
+﻿namespace Minecraft_Realms_Emulator.Entities
 {
-    [Keyless]
     public class Connection
     {
+        public int Id { get; set; }
         public World World { get; set; }
         public string Address { get; set; } = string.Empty;
         public bool PendingUpdate { get; set; }
---
+++ Minecraft-Realms-Emulator/Migrations/20240218160304_Connections_Id.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Minecraft_Realms_Emulator.Migrations
+{
+    /// <inheritdoc />
+    public partial class Connections_Id : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Connections",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Connections",
+                table: "Connections",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Connections",
+                table: "Connections");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Connections");
+        }
+    }
+}
