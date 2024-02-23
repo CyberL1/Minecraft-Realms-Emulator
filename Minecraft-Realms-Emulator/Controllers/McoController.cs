4e72381a50762fa44ddc408d4140d6c0a63f3c26
CyberL1 <cyber.hf18@gmail.com>
2024-02-23 14:40:49 +0100

    Move mco related controllers under McoController.cs

---
+++ Minecraft-Realms-Emulator/Controllers/McoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Minecraft_Realms_Emulator.Controllers.Mco
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class McoController : ControllerBase
+    {
+        [HttpGet("available")]
+        public bool GetAvailable()
+        { 
+            return true;
+        }
+
+        [HttpGet("client/compatible")]
+        public string GetCompatible()
+        {
+            return Compatility.COMPATIBLE.ToString();
+        }
+    }
+}
