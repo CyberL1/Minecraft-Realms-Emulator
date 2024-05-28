b91b1dbe6a3595c5a18d34cfba1b802ee2c86936
CyberL1 <mateusz.hf18@gmail.com>
2024-05-28 11:48:10 +0200

    feat: admin panel

---
+++ Minecraft-Realms-Emulator/Controllers/Admin/ServersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Minecraft_Realms_Emulator.Attributes;
+using Minecraft_Realms_Emulator.Data;
+using Minecraft_Realms_Emulator.Entities;
+
+namespace Minecraft_Realms_Emulator.Controllers.Admin
+{
+    [Route("api/admin/[controller]")]
+    [ApiController]
+    [RequireAdminKey]
+    public class ServersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ServersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<Configuration> GetConfiguration()
+        {
+            var worlds = _context.Worlds.ToList();
+
+            return Ok(worlds);
+        }
+    }
+}
---
+++ Panel/Classes/World.cs
@@ -0,0 +1,19 @@
+﻿namespace Panel.Classes
+{
+    public class World
+    {
+        public int Id { get; set; }
+        public string? Owner { get; set; }
+        public string? OwnerUUID { get; set; }
+        public string? Name { get; set; }
+        public string? Motd { get; set; }
+        public string State { get; set; } = null!;
+        public string WorldType { get; set; } = null!;
+        public int MaxPlayers { get; set; }
+        public string? MinigameName { get; set; }
+        public int? MinigameId { get; set; }
+        public string? MinigameImage { get; set; }
+        public int ActiveSlot { get; set; }
+        public bool Member { get; set; }
+    }
+}
