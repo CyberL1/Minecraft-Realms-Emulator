049014611c93c8ba4756e439e1e111e542e61057
CyberL1 <cyber.hf18@gmail.com>
2024-05-30 14:42:18 +0200

    feat(panel): log stream stop funtion

--- Panel/wwwroot/js/serverLogsStream.js
+++ Panel/wwwroot/js/serverLogsStream.js
@@ -1,9 +1,13 @@
 ﻿window.serverLogsStream = {
+    logs: "x",
+
     start: (dotNetObject, serverId) => {
-        const logs = new EventSource(`http://localhost:8080/api/admin/servers/${serverId}/logs`, { withCredentials: true });
+        logs = new EventSource(`http://localhost:8080/api/admin/servers/${serverId}/logs`, { withCredentials: true });
 
         logs.onmessage = event => {
             dotNetObject.invokeMethodAsync("ReceiveLog", event.data);
         }
-    }
+    },
+
+    stop: () => logs.close()
 }
