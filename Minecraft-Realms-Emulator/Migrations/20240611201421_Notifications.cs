afb6a4730c82a181ae0a22895457fefe080b77af
CyberL1 <mateusz.hf18@gmail.com>
2024-06-11 22:41:05 +0200

    feat: notifications

---
+++ Minecraft-Realms-Emulator/Migrations/20240611201421_Notifications.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Minecraft_Realms_Emulator.Migrations
+{
+    /// <inheritdoc />
+    public partial class Notifications : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notifications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    NotificationUuid = table.Column<string>(type: "text", nullable: false),
+                    Dismissable = table.Column<bool>(type: "boolean", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    Title = table.Column<JsonDocument>(type: "jsonb", nullable: true),
+                    Message = table.Column<JsonDocument>(type: "jsonb", nullable: false),
+                    Image = table.Column<string>(type: "text", nullable: true),
+                    UrlButton = table.Column<JsonDocument>(type: "jsonb", nullable: true),
+                    Url = table.Column<string>(type: "text", nullable: true),
+                    ButtonText = table.Column<JsonDocument>(type: "jsonb", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notifications", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notifications");
+        }
+    }
+}
