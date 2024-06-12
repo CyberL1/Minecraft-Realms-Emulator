4f0b9a5d6f4e38465e2d0777ee944c2fe203e402
CyberL1 <cyber.hf18@gmail.com>
2024-06-12 10:06:48 +0200

    feat: don't send seen notifications

---
+++ Minecraft-Realms-Emulator/Entities/SeenNotification.cs
@@ -0,0 +1,9 @@
+﻿namespace Minecraft_Realms_Emulator.Entities
+{
+    public class SeenNotification
+    {
+        public int Id { get; set; }
+        public string PlayerUUID { get; set; } = null!;
+        public string NotificationUUID { get; set; } = null!;
+    }
+}
--- Minecraft-Realms-Emulator/Modes/External/NotificationsController.cs
+++ Minecraft-Realms-Emulator/Modes/External/NotificationsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Minecraft_Realms_Emulator.Attributes;
 using Minecraft_Realms_Emulator.Data;
+using Minecraft_Realms_Emulator.Entities;
 using Minecraft_Realms_Emulator.Responses;
 
 namespace Minecraft_Realms_Emulator.Modes.External
@@ -20,15 +21,25 @@ namespace Minecraft_Realms_Emulator.Modes.External
         [HttpGet]
         public ActionResult<NotificationsResponse> GetNotifications()
         {
+            string cookie = Request.Headers.Cookie;
+            string playerUUID = cookie.Split(";")[0].Split(":")[2];
+
             List<NotificationResponse> notifications = [];
 
-            foreach (var notification in _context.Notifications)
+            foreach (var notification in _context.Notifications.ToList())
             {
+                var seen = _context.SeenNotifications.Any(n => n.PlayerUUID == playerUUID && n.NotificationUUID == notification.NotificationUuid);
+
+                if (seen)
+                {
+                    continue;
+                }
+
                 NotificationResponse notificationResponse = new()
                 {
                     NotificationUuid = notification.NotificationUuid,
                     Dismissable = notification.Dismissable,
-                    Seen = false,
+                    Seen = seen,
                     Type = notification.Type,
                     Title = notification.Title,
                     Message = notification.Message,
@@ -47,6 +58,45 @@ namespace Minecraft_Realms_Emulator.Modes.External
             };
 
             return Ok(notificationsResponse);
+        }
+
+        [HttpPost("seen")]
+        public ActionResult Seen(string[] notificationIds)
+        {
+            string cookie = Request.Headers.Cookie;
+            string playerUUID = cookie.Split(";")[0].Split(":")[2];
+
+            if (notificationIds.Length == 0)
+            {
+                ErrorResponse errorResponse = new()
+                {
+                    ErrorCode = 400,
+                    ErrorMsg = "Nothing to mark as seen"
+                };
+
+                return BadRequest(errorResponse);
+            }
+
+            foreach (var notificationId in notificationIds)
+            {
+                SeenNotification notificationSeen = new()
+                {
+                    PlayerUUID = playerUUID,
+                    NotificationUUID = notificationId
+                };
+
+                _context.SeenNotifications.Add(notificationSeen);
+            }
+
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPost("dismiss")]
+        public ActionResult Dismiss(string[] notificationIds)
+        {
+            return NoContent();
         }
     }
 }
