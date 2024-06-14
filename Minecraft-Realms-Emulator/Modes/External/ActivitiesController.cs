f9f07756ace2d6ece47148915b3ef3fbbe348eec
CyberL1 <mateusz.hf18@gmail.com>
2024-06-14 09:26:55 +0200

    feat: online player list

---
+++ Minecraft-Realms-Emulator/Modes/External/ActivitiesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Minecraft_Realms_Emulator.Attributes;
+using Minecraft_Realms_Emulator.Responses;
+
+namespace Minecraft_Realms_Emulator.Modes.External
+{
+    [Route("modes/external/[controller]")]
+    [ApiController]
+    [RequireMinecraftCookie]
+    public class ActivitiesController : ControllerBase
+    {
+        [HttpGet("liveplayerlist")]
+        public ActionResult<LivePlayerListsResponse> GetLivePlayerList()
+        {
+            LivePlayerListsResponse response = new()
+            {
+                Lists = []
+            };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ Minecraft-Realms-Emulator/Responses/LivePlayerListsResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Minecraft_Realms_Emulator.Responses
+{
+    public class LivePlayerListsResponse
+    {
+        public List<LivePlayerList> Lists { get; set; } = [];
+    }
+
+    public class LivePlayerList
+    {
+        public int ServerId { get; set; }
+        public string PlayerList { get; set; } = null!;
+    }
+}
