cb5a9e8422aa9bd50f5da49a67007395759c1723
CyberL1 <cyber.hf18@gmail.com>
2025-10-30 17:51:53 +0100

    refactor: remove unused empty function

--- Minecraft-Realms-Emulator/Attributes/RequireMinecraftCookieAttribute.cs
+++ Minecraft-Realms-Emulator/Attributes/RequireMinecraftCookieAttribute.cs
@@ -3,10 +3,6 @@
     [AttributeUsage(AttributeTargets.Class)]
     public class RequireMinecraftCookieAttribute : Attribute
     {
-        public RequireMinecraftCookieAttribute()
-        {
-        }
-
         public bool HasMinecraftCookie(string cookie)
         {
             return cookie.Contains("sid") && cookie.Contains("user") && cookie.Contains("version");
