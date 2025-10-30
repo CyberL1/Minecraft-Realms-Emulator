725f48d92ef016548ae31e365765826c273bee0c
CyberL1 <cyber.hf18@gmail.com>
2025-10-30 18:03:02 +0100

    refactor: make slot settings an array

--- Minecraft-Realms-Emulator/Responses/SlotResponse.cs
+++ Minecraft-Realms-Emulator/Responses/SlotResponse.cs
@@ -1,14 +1,11 @@
-﻿namespace Minecraft_Realms_Emulator.Responses
+﻿using Minecraft_Realms_Emulator.Objects;
+
+namespace Minecraft_Realms_Emulator.Responses
 {
     public class SlotResponse
     {
         public int SlotId { get; set; }
-        public SlotSettingsResponse Settings { get; set; } = null!;
+        public List<SlotSettingObject> Settings { get; set; } = [];
         public string Options { get; set; } = null!;
-
-        public SlotResponse()
-        {
-            Settings = new SlotSettingsResponse();
-        }
     }
 }
