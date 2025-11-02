19031bbea75706636626766a1a95ec36b40b116d
CyberL1 <cyber.hf18@gmail.com>
2025-11-02 12:36:57 +0100

    chore: remove unused slot options, make `GameMode` and `Difficulty` enums

---
+++ Minecraft-Realms-Emulator/Enums/DifficultyEnum.cs
@@ -0,0 +1,9 @@
+namespace Minecraft_Realms_Emulator.Enums;
+
+public enum DifficultyEnum
+{
+    Peaceful = 0,
+    Easy = 1,
+    Normal = 2,
+    Hard = 3,
+}
---
+++ Minecraft-Realms-Emulator/Migrations/20251102113151_Slots_unused_options.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Minecraft_Realms_Emulator.Migrations
+{
+    /// <inheritdoc />
+    public partial class Slots_unused_options : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CommandBlocks",
+                table: "Slots");
+
+            migrationBuilder.DropColumn(
+                name: "Pvp",
+                table: "Slots");
+
+            migrationBuilder.DropColumn(
+                name: "SpawnAnimals",
+                table: "Slots");
+
+            migrationBuilder.DropColumn(
+                name: "SpawnMonsters",
+                table: "Slots");
+
+            migrationBuilder.DropColumn(
+                name: "SpawnNPCs",
+                table: "Slots");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CommandBlocks",
+                table: "Slots",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Pvp",
+                table: "Slots",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "SpawnAnimals",
+                table: "Slots",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "SpawnMonsters",
+                table: "Slots",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "SpawnNPCs",
+                table: "Slots",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
