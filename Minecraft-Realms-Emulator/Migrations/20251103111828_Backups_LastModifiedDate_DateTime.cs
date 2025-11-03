fc1e86392680b2372aad78e6bd9be705c7ce8ca5
CyberL1 <cyber.hf18@gmail.com>
2025-11-03 12:57:27 +0100

    fix: make `LastModifiedDate` column in `Backups` table `DateType` type

---
+++ Minecraft-Realms-Emulator/Migrations/20251103111828_Backups_LastModifiedDate_DateTime.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Minecraft_Realms_Emulator.Migrations
+{
+    /// <inheritdoc />
+    public partial class Backups_LastModifiedDate_DateTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn("LastModifiedDate", "Backups");
+            migrationBuilder.AddColumn<DateTime>("LastModifiedDate", "Backups", defaultValueSql: "now()");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn("LastModifiedDate", "Backups");
+            migrationBuilder.AddColumn<long>("LastModifiedDate", "Backups");
+        }
+    }
+}
