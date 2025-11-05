fdad55379dccf73e73403cb9a2eba432dbb49e4e
CyberL1 <cyber.hf18@gmail.com>
2025-11-05 21:04:11 +0100

    chore: rename `RecipeintUUID` column to `RecipientUuid`, fixing a typo

---
+++ Minecraft-Realms-Emulator/Migrations/20251105200205_Invites_RecipientUuid_rename.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Minecraft_Realms_Emulator.Migrations
+{
+    /// <inheritdoc />
+    public partial class Invites_RecipientUuid_rename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RecipeintUUID",
+                table: "Invites",
+                newName: "RecipientUuid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RecipientUuid",
+                table: "Invites",
+                newName: "RecipeintUUID");
+        }
+    }
+}
