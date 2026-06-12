a7936700fb70b0241208469b3911426187cfe772
CyberL1 <cyber.hf18@gmail.com>
2026-06-12 13:53:49 +0200

    feat(Core): add `/mco` controller

---
+++ Core/Controllers/McoController.cs
@@ -0,0 +1,21 @@
+using Core.Enums;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class McoController : Controller
+{
+    [HttpGet("available")]
+    public ActionResult<bool> GetAvailable()
+    {
+        return Ok(true);
+    }
+
+    [HttpGet("client/compatible")]
+    public ActionResult<string> GetClientCompatible()
+    {
+        return Ok(nameof(CompatibleVersionResponse.COMPATIBLE));
+    }
+}
