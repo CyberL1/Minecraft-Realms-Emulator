2ca2e0402a6906c029f99207c95f6ae72bdfd1e7
CyberL1 <cyber.hf18@gmail.com>
2026-06-15 22:36:23 +0200

    fix(Core): mark `Realm.ActiveSlot` nullable

--- Core/Entities/Realm.cs
+++ Core/Entities/Realm.cs
@@ -12,6 +12,6 @@ public class Realm
     public required List<Slot> Slots { get; set; }
     public required string WorldType { get; set; }
     public required int ActiveSlotId { get; set; }
-    public required Slot ActiveSlot { get; set; }
+    public Slot? ActiveSlot { get; set; }
     public Realm? ParentWorld { get; set; }
 }
