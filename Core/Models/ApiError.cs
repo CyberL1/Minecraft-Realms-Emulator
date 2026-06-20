92c23f16f9725226cc12a77595d26448eac134a7
CyberL1 <cyber.hf18@gmail.com>
2026-06-20 08:35:50 +0200

    fix(Core): make `ApiError` errors work

--- Core/Models/ApiError.cs
+++ Core/Models/ApiError.cs
@@ -2,14 +2,14 @@ namespace Core.Models;
 
 public class ApiError
 {
-    public int ErrorCode;
-    public string ErrorMsg;
-
     private ApiError(int errorCode, string errorMsg)
     {
         ErrorCode = errorCode;
         ErrorMsg = errorMsg;
     }
+
+    public int ErrorCode { get; init; }
+    public string ErrorMsg { get; init; }
 
     public static ApiError WorldNotFound => new(404, "World not found");
     public static ApiError NotAWorldMember => new(403, "Not a world member"); // TODO: Check if this is correct
